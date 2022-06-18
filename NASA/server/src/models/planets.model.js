3ef846f4ddae6cf2218ce051fa8b0abf4e3a1da4
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-06-18 13:23:09 +0200

    added automation scripts to NPM

--- NASA/server/src/models/planets.model.js
+++ NASA/server/src/models/planets.model.js
@@ -1,3 +1,50 @@
-const planets = []
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const Path = require("path");
+const habitablePlanets = [];
 
-module.exports = planets;
+/*IT IS GOOD IDEA TO STREAM LARGE DATA SETS, NODE IS VERY GOOD FOR STREAMING DATA
+ */
+
+function isHabitable(data) {
+  const { koi_disposition, koi_insol, koi_prad } = data;
+  if (
+    koi_disposition == "CONFIRMED" &&
+    koi_insol > 0.36 &&
+    koi_insol < 1.11 &&
+    koi_prad < 1.6
+  ) {
+    habitablePlanets.push(data);
+  }
+  return;
+}
+
+const loadPlanetData = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(Path.join(__dirname, "..", "..", 'data', 'kepler_data.csv'))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        isHabitable(data);
+      })
+      .on("error", (err) => {
+        console.error(err);
+        reject(error);
+      })
+      .on("end", () => {
+        console.table(habitablePlanets.map((result) => result.kepler_name));
+        console.log();
+        console.info(`${habitablePlanets.length} Habitable Planets found!`);
+        resolve();
+      });
+  });
+};
+
+module.exports = {
+  loadPlanetData,
+  planets: habitablePlanets,
+};
--- NASA/server/src/server.js
+++ NASA/server/src/server.js
@@ -1,11 +1,21 @@
 const http = require("http");
 
-const app = require("./app")
+const app = require("./app");
+
+const { loadPlanetData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log("Server is listening on port " + PORT);
-});
+async function startServer (){
+  await loadPlanetData();
+
+  server.listen(PORT, () => {
+    console.log("Server is listening on port " + PORT);
+  });
+}
+
+startServer()
+
+
